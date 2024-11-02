e6a3da5ddd6633ff7e72cea9821c833c88b85178
lqasemzadeh <lqasemzadeh@gmail.com>
2024-11-02 14:59:35 +0100

    Enhance Accordion component with state management
    Introduce `useState` to control Accordion's open/close state and toggle icons based on the state. Update Projects page to instantiate multiple Accordions with pre-defined titles and content for better user interaction.

--- src/pages/Accordion.js
+++ src/pages/Accordion.js
@@ -1,19 +1,23 @@
-import React from 'react';
-import Button from "@/components/Button";
+import React, {useState} from 'react';
 import { IoIosArrowBack } from "react-icons/io";
+import { IoIosArrowForward } from "react-icons/io";
+
 
 
 
 const Accordion = () => {
+
+    const [accordionOpen, setAccordionOpen] = useState(false);
+
     return (
         <div className="py-2">
-            <button className="flex justify-between w-full">
+            <button onClick={() => setAccordionOpen(!accordionOpen)} className="flex justify-between w-full focus:outline-none">
                 <span>پنل پیامک</span>
-                <IoIosArrowBack />
+                {accordionOpen ? <IoIosArrowForward /> : <IoIosArrowBack />}
             </button>
-            <div>
+            <div className={`overflow-hidden transition-all duration-300 ease-in-out text-slate-600 text-sm ${accordionOpen ? 'max-h-[300px]' : 'h-0'}`}>
                 <div>
-
+                    شرکت برج الکترونیک شیراز سرویس‌های پیام‌دهی تحت وب با واسط کاربری ساده و کارآمد برای سازمان‌ها ارائه می‌دهد.
                 </div>
             </div>
         </div>
