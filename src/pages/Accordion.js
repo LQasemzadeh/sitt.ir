61d9a482b2f1a9e826f2d317d54d912fd9358ec7
lqasemzadeh <lqasemzadeh@gmail.com>
2024-11-02 15:04:35 +0100

    Refactor Accordion component and update project details
    Refactor the Accordion component to accept dynamic titles and content through props. Update the project descriptions in the Projects page for more diverse and accurate information.

--- src/pages/Accordion.js
+++ src/pages/Accordion.js
@@ -5,20 +5,18 @@ import { IoIosArrowForward } from "react-icons/io";
 
 
 
-const Accordion = () => {
+const Accordion = ({title, answer}) => {
 
     const [accordionOpen, setAccordionOpen] = useState(false);
 
     return (
         <div className="py-2">
             <button onClick={() => setAccordionOpen(!accordionOpen)} className="flex justify-between w-full focus:outline-none">
-                <span>پنل پیامک</span>
+                <span>{title}</span>
                 {accordionOpen ? <IoIosArrowForward /> : <IoIosArrowBack />}
             </button>
             <div className={`overflow-hidden transition-all duration-300 ease-in-out text-slate-600 text-sm ${accordionOpen ? 'max-h-[300px]' : 'h-0'}`}>
-                <div>
-                    شرکت برج الکترونیک شیراز سرویس‌های پیام‌دهی تحت وب با واسط کاربری ساده و کارآمد برای سازمان‌ها ارائه می‌دهد.
-                </div>
+                <div>{answer}</div>
             </div>
         </div>
     );
