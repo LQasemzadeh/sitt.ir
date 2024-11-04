53a6c43dc9031599a96947ca60af8b614959fa0f
lqasemzadeh <lqasemzadeh@gmail.com>
2024-11-04 18:11:03 +0100

    Enhance pricing section layout and styling
    Adjusted bottom margin and grid for better responsiveness. Added price display with background styling in each pricing plan. Increased readability by removing secondary text color from plan descriptions.

--- src/pages/Price.js
+++ src/pages/Price.js
@@ -62,17 +62,24 @@ const Price = () => {
                     انتخاب پنل پیامکی
                 </h1>
             </header>
-            <div data-aos="fade-down" className="mb-4">
+            <div data-aos="fade-down" className="mb-10">
                 <p className="text-center md:text-right">
                     سرويس‌های پیام‌دهی تحت وب با واسط کاربری ساده و کارآمد برای سازمان‌ها و شرکت‌ها
                 </p>
             </div>
-            <div className="grid grid-cols-3 gap-8">
+            <div className="grid sm:grid-cols-3 gap-8">
                 {pricingPlans.map((plan) =>(
                     <div key={plan.title} className="relative border border-slate-200 shadow-lg p-8 bg-white rounded-2xl">
                         <h3 className="text-lg font-semibold leading-5">{plan.title}</h3>
                         {plan.mostPopular && <p className="absolute -top-3 bg-primary text-secondary px-3 py-0.5 text-sm font-semibold tracking-wide rounded-full shadow-md">پرفروش‌ترین</p>}
-                        <p className="mt-4 text-sm text-slate-700 leading-6">{plan.description}</p>
+                        <p className="mt-4 text-sm leading-6">{plan.description}</p>
+                        <div className="-mx-6 mt-4 rounded-lg bg-primary bg-opacity-10 p-6">
+                            <p className="text-sm font-semibold text-secondary">
+                                <span>{plan.price}</span>
+                                <span>{plan.currency}</span>
+                                <span>{plan.frequency}</span>
+                            </p>
+                        </div>
                     </div>
 
                 ))}
