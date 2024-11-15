930147c9b8c77b713311c6afe09ff3d63e962fe0
lqasemzadeh <lqasemzadeh@gmail.com>
2024-11-15 09:28:46 +0100

    Add new Contact page
    Created a new Contact page with a basic form in React. This includes a form with three input fields for user contact information. The page structure sets the foundation for further enhancement and styling.

---
+++ src/pages/Contact.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div>
+            <h2>Contact Us</h2>
+            <form action="">
+                <div>
+                    <label htmlFor=""></label>
+                    <input type="text" placeholder="Name"/>
+                </div>
+                <div>
+                    <label htmlFor=""></label>
+                    <input type="text" placeholder="Name"/>
+                </div>
+                <div>
+                    <label htmlFor=""></label>
+                    <input type="text" placeholder="Name"/>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default Contact;
